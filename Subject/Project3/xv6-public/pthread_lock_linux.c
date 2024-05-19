cac2737160e29f56b7b77e9cd1eef353ce78ab5e
grand6135 <2020028377@hanyang.ac.kr>
2024-05-19 02:15:18 +0000

    Locking and Unlokcing

--- Subject/Project3/xv6-public/pthread_lock_linux.c
+++ Subject/Project3/xv6-public/pthread_lock_linux.c
@@ -1,48 +1,80 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define NUM_ITERS 10000
+#define NUM_THREADS 100
+
 int shared_resource = 0;
+int choosing[NUM_THREADS];
+int number[NUM_THREADS];
 
-#define NUM_ITERS 10
-#define NUM_THREADS 10
+void lock(int tid) {
+    choosing[tid] = 1;
 
-void lock();
-void unlock();
+    // Find the maximum ticket number
+    int max = 0;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (number[i] > max) {
+            max = number[i];
+        }
+    }
 
-void lock()
-{
+    number[tid] = max + 1;
+    choosing[tid] = 0;
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (i == tid) {
+            continue;
+        }
+
+        while (choosing[i]) {
+            // busy-wait
+        }
+
+        while (number[i] != 0 &&
+               (number[i] < number[tid] || (number[i] == number[tid] && i < tid))) {
+            // busy-wait
+        }
+    }
 }
 
-void unlock()
-{
+void unlock(int tid) {
+    number[tid] = 0;
 }
 
 void* thread_func(void* arg) {
     int tid = *(int*)arg;
-    
-    lock();
-    
-        for(int i = 0; i < NUM_ITERS; i++)    shared_resource++;
-    
-    unlock();
-    
+
+    lock(tid);
+
+    for (int i = 0; i < NUM_ITERS; i++) {
+        shared_resource++;
+    }
+
+    unlock(tid);
+
     pthread_exit(NULL);
 }
 
 int main() {
-    pthread_t threads[n];
-    int tids[n];
-    
-    for (int i = 0; i < n; i++) {
+    pthread_t threads[NUM_THREADS];
+    int tids[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        choosing[i] = 0;
+        number[i] = 0;
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
         tids[i] = i;
         pthread_create(&threads[i], NULL, thread_func, &tids[i]);
     }
-    
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
 
     printf("shared: %d\n", shared_resource);
-    
+
     return 0;
 }
